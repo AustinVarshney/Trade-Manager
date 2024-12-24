264a218afa020492ed69fe29d2d17d8408f7ff90
Austin Varshney <austinvarshney@gmail.com>
2024-12-24 10:18:36 +0530

    Removed trailing char

--- frontend/environment.js
+++ frontend/environment.js
@@ -1,9 +1,9 @@
 const isProd = true;
 
 const links = {
-    dashboard: isProd ? "https://zerodha-project-dashboard-f0lb.onrender.com/" : "http://localhost:5173",
+    dashboard: isProd ? "https://zerodha-project-dashboard-f0lb.onrender.com" : "http://localhost:5173",
     backend : isProd ? "https://zerodha-project-backend.onrender.com" : "http://localhost:8080",
-    frontend : isProd ? "https://zerodha-project-frontend-xysp.onrender.com/" : "http://localhost:5174",
+    frontend : isProd ? "https://zerodha-project-frontend-xysp.onrender.com" : "http://localhost:5174",
 }
 
 export default links;
