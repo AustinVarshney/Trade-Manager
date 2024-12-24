53917a1b1c1b00d98bbccc49283e018782988521
Austin Varshney <austinvarshney@gmail.com>
2024-12-24 09:56:00 +0530

    Removed chunkSize

--- dashboard/vite.config.cjs
+++ dashboard/vite.config.cjs
@@ -5,7 +5,6 @@ module.exports = defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    chunkSizeWarningLimit: 1000 // Set the limit to 1000 KB or any value you prefer
   },
   server: {
     historyApiFallback: true, // This is typically for dev mode but let's ensure it's set.
